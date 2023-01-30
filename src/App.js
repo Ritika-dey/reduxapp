630703434db460d9b77df3454e4674485a458e0d
Ritika Dey <ritidey098@gmail.com>
2023-01-30 12:47:29 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import {useSelector, useDispatch} from 'react-redux'
+import {incNum, decNum} from './actions/index'
 
+// usedespatch is trigering the action
 function App() {
+  const myState = useSelector((state) => state.counter)
+  const dispatch = useDispatch()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className='container'>
+        <h1>Counter app using React + Redux</h1>
+        <div className='quantity'>
+          <a className='quantity__minus' title='decrement' onClick={() => {dispatch(decNum())}}><span>-</span></a>
+          <input name='quantity' type="text" className='quantity__input' value={myState} />
+          <a className='quantity__plus' title='increment' onClick={() => {dispatch(incNum(5))}}><span>+</span></a>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
